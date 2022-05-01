8e6b6e9029202403561776507c3ab89144d1e293
ffalpha <kalanam217@gmail.com>
2022-05-01 15:15:28 +0530

    Add env

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {ChatEngine} from 'react-chat-engine';
+
+
+import './App.css';
+
+
+const App = () =>{
+    return(
+        <ChatEngine 
+          height="100vh"
+          projetID=""
+          userName=""
+          userSecret=""
+        
+        />
+    )
+}
+
+
+export default App;
